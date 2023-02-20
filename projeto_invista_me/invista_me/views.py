9e6ec7b499f2859a5c649fd233bcea3d43bcb265
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-20 13:19:24 +0000

    commit

--- projeto_invista_me/invista_me/views.py
+++ projeto_invista_me/invista_me/views.py
@@ -30,3 +30,9 @@ def investimentos(request):
         'dados': Investimento.objects.all()
     }
     return render(request, 'investimentos/investimentos.html', context=dados)
+
+def detalhe(request, id_investimento):
+    dados = {
+        'dados': Investimento.objects.get(pk=id_investimento)
+    }
+    return render(request,'invesstimentos/detalhe.html', dados)
--- projeto_invista_me/projeto_invista_me/urls.py
+++ projeto_invista_me/projeto_invista_me/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("contato/", views.contato, name='contato'),
     path("minha_historia/",views.minha_historia, name='minha_historia'),
     path("novo_investimento/", views.novo_investimento, name='novo_investimento'),
-    path("investimento_registrado/", views.investimento_registrado, name='investimento_registrado')
+    path("investimento_registrado/", views.investimento_registrado, name='investimento_registrado'),
+    path('/<int:id_investimento>', views.detalhe, name='detalhe')
 ]
