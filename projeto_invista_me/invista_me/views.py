395dd992a71446d7892198ebf7a84a11fa4c7c7f
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-21 18:07:13 +0000

    commit

--- projeto_invista_me/invista_me/views.py
+++ projeto_invista_me/invista_me/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Investimento
 from .forms import InvestimentoForm
 
@@ -41,8 +40,35 @@ def detalhe(request, id_investimento):
     return render(request, 'investimentos/detalhe.html', dados)
 
 def criar(request):
-    investimento_form = InvestimentoForm()
-    formulario = {
-        'formulario': investimento_form
-    }
-    return render(request, 'investimentos/novo_investimento.html', context=formulario)
+    if request.method == 'POST':
+        investimento_form = InvestimentoForm(request.POST)
+        if investimento_form.is_valid():
+            investimento_form.save()
+        return redirect('investimentos')
+    else:
+        investimento_form = InvestimentoForm()
+        formulario = {
+            "formulario": investimento_form
+        }
+        return render(request, "investimentos/novo_investimento.html", context=formulario)
+
+def editar(request, id_investimento):
+    investimento = Investimento.objects.get(pk=id_investimento)
+    #novo investimento/1 -> GET
+    if request.method == 'GET':
+        formulario = InvestimentoForm(instance=investimento)
+        return render(request, 'investimentos/novo_investimento.html', {'formulario': formulario})
+    #caso requisicao seja POST
+    else:
+        formulario = InvestimentoForm(request.POST, instance=investimento)
+        if formulario.is_valid():
+            formulario.save()
+        return redirect('investimentos')
+    
+def excluir(request, id_investimento):
+    investimento = Investimento.objects.get(pk=id_investimento)
+    if request.method == 'POST':
+        investimento.delete()
+        return redirect('investimentos')
+    return render(request, 'investimentos/confirmar_exclusao.html', {'item': investimento})
+    
--- projeto_invista_me/projeto_invista_me/urls.py
+++ projeto_invista_me/projeto_invista_me/urls.py
@@ -19,9 +19,10 @@ from invista_me import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.investimentos),
-    path('novo_investimento/', 
-         views.criar, name='novo_investimento'),
+    path('', views.investimentos, name='investimentos'),
+    path('novo_investimento/', views.criar, name='novo_investimento'),
+    path('novo_investimento/<int:id_investimento>', views.editar, name='editar'),
+    path('excluir_investimento/<int:id_investimento>', views.excluir, name='excluir'),
     path('/<int:id_investimento>', views.detalhe, name='detalhe')
     
 ]
