648cd2b9642a5c21b6c1510cad5e05ad54ce1bbd
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-22 18:33:25 +0000

    commit inicial

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -126,3 +126,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+LOGIN_REDIRECT_URL = 'investimentos'
+LOGIN_URL = 'login'
--- projeto_invista_me/urls.py
+++ projeto_invista_me/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('conta/', usuarios_views.novo_usuario, name='novo_usuario'),
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
     path('', views.investimentos, name='investimentos'),
     path('novo_investimento/', views.criar, name='novo_investimento'),
     path('novo_investimento/<int:id_investimento>', views.editar, name='editar'),
