a9443fa41133be7b104c4b8136bdcbc68b59c7f1
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-02-23 13:44:33 +0000

    commit

--- projeto_invista_me/invista_me/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Investimento
-
-admin.site.register(Investimento)
-# Register your models here.
--- projeto_invista_me/invista_me/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Investimento
-
-class InvestimentoForm(ModelForm):
-    class Meta:
-        model = Investimento
-        fields = '__all__'
-        
