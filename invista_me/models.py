bccfdbb872dac4a9c3f1f76d937e17ab6eabf5b5
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-01 14:51:14 +0000

    commit

--- invista_me/models.py
+++ invista_me/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from djmoney.models.fields import MoneyField
+
 
 '''
 * investimento
@@ -11,9 +11,7 @@ from djmoney.models.fields import MoneyField
 
 class Investimento(models.Model):
     investimento = models.TextField(max_length=255)
-    #valor = models.FloatField()
+    valor = models.FloatField()
     pago = models.BooleanField(default=False)
     data = models.DateField(default=datetime.now())
-    nome = models.CharField(max_length=100)
-    valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
-    
+            
