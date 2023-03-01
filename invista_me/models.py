035750e6a7d3f1cf581dd8b1d54563ef18a92ccc
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-01 14:40:40 +0000

    commit djmoney

--- invista_me/models.py
+++ invista_me/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from djmoney.models.fields import MoneyField
 
 '''
 * investimento
@@ -10,7 +11,9 @@ from datetime import datetime
 
 class Investimento(models.Model):
     investimento = models.TextField(max_length=255)
-    valor = models.FloatField()
+    #valor = models.FloatField()
     pago = models.BooleanField(default=False)
     data = models.DateField(default=datetime.now())
-            
+    nome = models.CharField(max_length=100)
+    valor = MoneyField(max_digits=14, decimal_places=2, default_currency='BRL')
+    
--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "usuarios",
+    "djmoney",
 ]
 
 MIDDLEWARE = [
