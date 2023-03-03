550a7a8876cc8b2ce19fc4e126efb378ec3368b6
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-03 19:33:28 +0000

    commit

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -117,6 +117,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+USE_THOUSAND_SEPARATOR = True
+
+THOUSAND_SEPARATOR = '.'
+
+DECIMAL_SEPARATOR = ','
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
