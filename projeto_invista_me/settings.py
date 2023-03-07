f6355904f40b1fe83d8284fe8637e6fc40cb2faf
S4m4r1t4N <emerson_guimaraes@hotmail.com>
2023-03-07 07:22:02 +0000

    csrf_origins

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY = "django-insecure-orwpgm7h!yrd$=+@9us&oi*%5iln3(^g39rk-p-ldbwp-#76qe
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+CSRF_TRUSTED_ORIGINS = ["bonusaplicacoeswebcomdjango-production-7b09.up.railway.app"]
+CORS_ORIGIN_WHITELIST = ["bonusaplicacoeswebcomdjango-production-7b09.up.railway.app",]
+CORS_ORIGIN_ALLOW_ALL = True
 
-ALLOWED_HOSTS = ["bonusaplicacoeswebcomdjango-production-7b09.up.railway.app"]
+ALLOWED_HOSTS = ["bonusaplicacoeswebcomdjango-production-7b09.up.railway.app",]
 
 
 # Application definition
